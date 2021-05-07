d69438c36b41f10510878b9d9fd45063647c1265
Nicolo John Davis <nicolodavis@gmail.com>
2021-05-07 22:54:03 +0800

    add automation field to state

--- src/state.ts
+++ src/state.ts
@@ -46,6 +46,8 @@ export interface State {
   // True if the latest state update was the result of actions
   // from a remote player.
   remote?: boolean;
+
+  automation: any;
 }
 
 export interface SeatInfo {
